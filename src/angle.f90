faa43ee0d2e1756670875f7426dba43c5d79edd3
Jeremy Roberts <jaroberts@ksu.edu>
2017-02-14 14:13:31 -0600

    First commit, an incomplete angle module.

---
+++ src/angle.f90
@@ -0,0 +1,45 @@
+module angle
+
+integer :: number_angles ! number angles per half space
+double precision, dimension(:) :: mu ! cosine of angles
+double precision, dimension(:) :: wt ! quadrature weight (sum to 1)
+
+integer, parameter :: GL = 1, DGL = 2
+
+contains
+
+subroutine initialize(n, option) 
+  integer, intent(in) :: n, option
+  integer :: i
+  number_angles = n
+  allocate(mu(number_angles), wt(number_angles))
+  
+  if (option .eq. GL) then
+    call fill_gl()
+  else if (option .eq. DGL) then
+    call fill_dgl()
+  else if
+    stop "Invalid quadrature option."
+  end if
+  
+end subroutine initialize
+
+subroutine finalize()
+  if (allocated(mu)) then
+    deallocate(mu)
+  end if
+  if (allocated(wt)) then
+    deallocate(wt)
+  end if
+end subroutine finalize
+
+subroutine fill_gl
+  ! put algorithm here to determine legendre zeros over -1, 1
+end subroutine fill_gl
+
+subroutine fill_dgl
+  ! shift and scale gl numbers
+end subroutine fill_dgl
+
+
+end module angle
