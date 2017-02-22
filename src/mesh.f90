f272b591d9cd622d21adf6ae6658f8cb1e1f73e2
Richard <rlreed@ksu.edu>
2017-02-22 15:40:25 -0600

    Updated spacing and removed a print statement

--- src/mesh.f90
+++ src/mesh.f90
@@ -7,29 +7,28 @@ integer, allocatable, dimension(:) :: mMap  ! Material within each cell
 contains
 
 subroutine create(fineMesh, courseMesh, materialMap)
-    integer, intent(in) :: fineMesh(:)
-    integer, intent(in) :: materialMap(:)
-    double precision, intent(in) :: courseMesh(:)
+  integer, intent(in) :: fineMesh(:)
+  integer, intent(in) :: materialMap(:)
+  double precision, intent(in) :: courseMesh(:)
     
-    double precision :: width  ! Total width of problem
-    double precision :: ddx  ! Temperary variable
-    integer :: n, c
-    n = size(fineMesh)  ! Number of course mesh regions
-    c = 1  ! counting variable
+  double precision :: width  ! Total width of problem
+  double precision :: ddx  ! Temperary variable
+  integer :: n, c
+  n = size(fineMesh)  ! Number of course mesh regions
+  c = 1  ! counting variable
     
-    nCells = sum(fineMesh)
-    allocate(dx(nCells), mMap(nCells))
+  nCells = sum(fineMesh)
+  allocate(dx(nCells), mMap(nCells))
     
-    do i = 1, n
-        ddx = (courseMesh(i+1) - courseMesh(i)) / fineMesh(i)
-        print *, ddx
-        do j = 1, fineMesh(i)
-            dx(c) = ddx
-            mMap(c) = materialMap(i)
-            c = c + 1
-        end do
+  do i = 1, n
+    ddx = (courseMesh(i+1) - courseMesh(i)) / fineMesh(i)
+    do j = 1, fineMesh(i)
+      dx(c) = ddx
+      mMap(c) = materialMap(i)
+      c = c + 1
     end do
-    width = courseMesh(n) - courseMesh(1)
+  end do
+  width = courseMesh(n) - courseMesh(1)
 end subroutine create
 
 end module mesh
