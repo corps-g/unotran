5c10e1f785972e38605fdd5a459f1134197293be
Richard <rlreed@ksu.edu>
2017-02-23 13:18:15 -0600

    Some minor updates to clean up the mesh class

--- src/mesh.f90
+++ src/mesh.f90
@@ -1,34 +1,34 @@
 module mesh
+  implicit none
+  integer :: nCells  ! Total number of cells in the mesh
+  double precision, allocatable, dimension(:) :: dx  ! Width of each cell
+  integer, allocatable, dimension(:) :: mMap  ! Material within each cell
 
-integer :: nCells  ! Total number of cells in the mesh
-double precision, allocatable, dimension(:) :: dx  ! Width of each cell
-integer, allocatable, dimension(:) :: mMap  ! Material within each cell
+  contains
 
-contains
-
-subroutine create(fineMesh, courseMesh, materialMap)
-  integer, intent(in) :: fineMesh(:)
-  integer, intent(in) :: materialMap(:)
-  double precision, intent(in) :: courseMesh(:)
-    
-  double precision :: width  ! Total width of problem
-  double precision :: ddx  ! Temperary variable
-  integer :: n, c
-  n = size(fineMesh)  ! Number of course mesh regions
-  c = 1  ! counting variable
-    
-  nCells = sum(fineMesh)
-  allocate(dx(nCells), mMap(nCells))
-    
-  do i = 1, n
-    ddx = (courseMesh(i+1) - courseMesh(i)) / fineMesh(i)
-    do j = 1, fineMesh(i)
-      dx(c) = ddx
-      mMap(c) = materialMap(i)
-      c = c + 1
+  subroutine create(fineMesh, courseMesh, materialMap)
+    integer, intent(in) :: fineMesh(:)
+    integer, intent(in) :: materialMap(:)
+    double precision, intent(in) :: courseMesh(:)
+      
+    double precision :: width  ! Total width of problem
+    double precision :: ddx  ! Temperary variable
+    integer :: n, c, i, j
+    n = size(fineMesh)  ! Number of course mesh regions
+    c = 1  ! counting variable
+      
+    nCells = sum(fineMesh)
+    allocate(dx(nCells), mMap(nCells))
+      
+    do i = 1, n
+      ddx = (courseMesh(i+1) - courseMesh(i)) / fineMesh(i)
+      do j = 1, fineMesh(i)
+        dx(c) = ddx
+        mMap(c) = materialMap(i)
+        c = c + 1
+      end do
     end do
-  end do
-  width = courseMesh(n) - courseMesh(1)
-end subroutine create
+    width = courseMesh(n) - courseMesh(1)
+  end subroutine create
 
 end module mesh
