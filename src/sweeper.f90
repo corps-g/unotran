0120cf8c0e002fc9a2c11cbee1262c7e7ffa4b70
Richard <rlreed@ksu.edu>
2017-02-28 15:14:53 -0600

    Cleaned up the sweeper file and added a subroutine to update the legendre moments used in the source

--- src/sweeper.f90
+++ src/sweeper.f90
@@ -3,31 +3,32 @@ module sweeper
   ! Assumes only vacuum conditions
   use material, only: sig_s, sig_t, vsig_f, chi, number_groups, number_legendre
   use mesh, only: dx, number_cells, mMap
-  use angle, only: number_angles
+  use angle, only: number_angles, p_leg, wt, mu
   use state, only: phi, psi, source, phistar, internal_source
   implicit none
   
   contains
-  ! Define phistar, P, source
+  ! Define source
   
   subroutine sweep()
-    ! Sweep in positive mu direction
-    integer :: c, a, g, l
-    double precision :: incoming = 0.0
+    integer :: c, a, g, l, an
+    double precision :: incoming, scat, invmu
     
     ! get the source array
     call update_source()
     
+    incoming = 0.0
+    ! Sweep in positive mu direction
     do c = 1, number_cells
       do a = 1, number_angles
         do g = 1, number_groups
-          invmu = dx(c) / (2 * abs(mu[a]))
+          invmu = dx(c) / (2 * abs(mu(a)))
           scat = 0.0
           do l = 0, number_legendre
-            scat = scat + (2 * l + 1) * P(l, a) * sig_s(mMap(c), l, g, g) * phistar(c, l, g)
+            scat = scat + (2 * l + 1) * p_leg(l, a) * sig_s(mMap(c), l, g, g) * phistar(c, l, g)
           end do
           !psi(c, a, g) = (incoming + invmu * (scat + source(c, a, g))) / (1 + sig_t(c, g) * invmu)
-          phi(c, g) = phi(c,g) + w[a] * (incoming + invmu * (scat + internal_source(c, a, g))) / (1 + sig_t(mMap(c), g) * invmu)
+          phi(c, g) = phi(c,g) + wt(a)*(incoming+invmu*(scat+internal_source(c, a, g)))/(1+sig_t(mMap(c), g)*invmu)
           incoming = 2 * psi(c, a, g) - incoming
         end do
       end do
@@ -36,16 +37,17 @@ module sweeper
     incoming = 0.0
     ! Sweep in negative mu direction
     do c = number_cells, 1, -1
-      do a = 1, number_angles
+      do a = number_angles, 1, -1
         do g = 1, number_groups
-          invmu = dx(c) / (2 * abs(mu[a]))
+          invmu = dx(c) / (2 * abs(mu(a)))
           scat = 0.0
-          do l = 0, nLegendre
-            scat = scat + (2 * l + 1) * P(l, a) * sig_s(mMap(c), l, g, g) * phistar(c, l, g)
+          do l = 0, number_legendre
+            scat = scat + (2 * l + 1) * p_leg(l, 2*number_angles - a + 1) * sig_s(mMap(c), l, g, g) * phistar(c, l, g)
           end do
           !psi(c, a, g) = (incoming + invmu * (scat + source(c, a, g))) / (1 + sig_t(c, g) * invmu)
-          phi(c, g) = phi(c,g) + w[a] * (incoming + invmu * (scat + internal_source(c, a, g))) / (1 + sig_t(mMap(c), g) * invmu)
-          incoming = 2 * psi(c, a, g) - incoming
+          an = 2 * number_angles - a + 1
+          phi(c, g) = phi(c,g) + wt(a) * (incoming + invmu * (scat + internal_source(c, an, g))) / (1 + sig_t(mMap(c), g) * invmu)
+          incoming = 2 * psi(c, an, g) - incoming
         end do
       end do
     end do
@@ -54,10 +56,12 @@ module sweeper
   end subroutine sweep
   
   subroutine update_source()
+    integer :: c, a, g, gp, l
     double precision :: out_scatter, fission
+    call update_phistar()
   
     do c = 1, number_cells
-      do a = 1, number_angles
+      do a = 1, number_angles * 2
         do g = 1, number_groups
           out_scatter = 0.0
           fission = 0.0
@@ -65,7 +69,7 @@ module sweeper
             ! Get the scattering source
             if (gp .ne. g) then  ! Only care about groups outside of the current group
               do l = 0, number_legendre
-                out_scatter = out_scatter + (2 * l + 1) * P(l, a) * sig_s(mMap(c), l, g, gp) * phistar(c, l, gp)
+                out_scatter = out_scatter + (2 * l + 1) * p_leg(l, a) * sig_s(mMap(c), l, g, gp) * phistar(c, l, gp)
               end do
             end if
             ! Get the fission source
@@ -78,5 +82,23 @@ module sweeper
       end do
     end do
   end subroutine update_source
+  
+  subroutine update_phistar()
+    integer :: c, l, g, a, an
+    do c = 1, number_cells
+      do l = 0, number_legendre
+        do g = 1, number_groups
+          phistar(c, l, g) = 0.0
+          do a = 1, number_angles
+            ! for positive mu
+            phistar(c, l, g) = phistar(c, l, g) + 0.5 * wt(a) * p_leg(l, a) * psi(c, a, g)
+            ! for negative mu
+            an = 2*number_angles - a + 1
+            phistar(c, l, g) = phistar(c, l, g) + 0.5 * wt(a) * p_leg(l, an) * psi(c, an, g)
+          end do
+        end do
+      end do
+    end do
+  end subroutine update_phistar
   
 end module sweeper
