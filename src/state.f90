bb25276512e52d09a132fb9577f74554ae215dee
Richard <rlreed@ksu.edu>
2017-02-28 10:09:28 -0600

    Making a state class to make containers for the various state variables i.e., phi and psi

---
+++ src/state.f90
@@ -0,0 +1,22 @@
+module state
+  use material, only: number_groups,number_legendre
+  use mesh, only: number_cells, mmap
+  use angle, only: number_angles
+  implicit none
+  double precision, allocatable, dimension(:,:) :: phi
+  double precision, allocatable, dimension(:,:,:) :: psi, source, phistar, internal_source
+  
+  save
+  
+  contains
+  
+  subroutine initialize_state()
+    allocate(phi(number_cells,number_groups))
+    allocate(psi(number_cells,number_angles,number_groups))
+    allocate(source(number_cells,number_angles,number_groups))
+    allocate(phistar(number_cells,number_legendre,number_groups))
+    allocate(internal_source(number_cells,number_angles,number_groups))
+    
+  end subroutine initialize_state
+  
+end module
