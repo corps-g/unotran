78a2a4e0c12304ce5cf06f0bffcf275eff2ac085
Richard <rlreed@ksu.edu>
2017-02-28 10:07:56 -0600

    Renamed nLegendre -> number_legendre and cleaned up the naming.  Made number_legendre publicly available

--- src/material.f90
+++ src/material.f90
@@ -1,44 +1,44 @@
 module material
   implicit none
-  integer :: nMaterials, nGroups, debugFlag
+  integer :: number_materials, number_groups, debugFlag, number_legendre
   double precision, allocatable, dimension(:) :: ebounds, velocity
   double precision, allocatable, dimension(:,:) :: sig_t, sig_f, vsig_f, chi
   double precision, allocatable, dimension(:,:,:,:) :: sig_s
-    
+  save
   contains
 
-  subroutine create(fileName)
+  subroutine create_material(fileName)
     character(len=*), intent(in) :: fileName
     character(1000) :: materialName
-    integer :: mat, group, groupp, L, nLegendre, dataPresent
+    integer :: mat, group, groupp, L, dataPresent
     double precision :: t, f, vf, c, energyFission, energyCapture, gramAtomWeight
     double precision, allocatable, dimension(:) :: array1
     
     ! Read the file parameters
     open(unit=5, file=fileName)
-    read(5,*) nMaterials, nGroups, debugFlag
-    allocate(ebounds(nGroups+1))
+    read(5,*) number_materials, number_groups, debugFlag
+    allocate(ebounds(number_groups + 1))
     read(5,*) ebounds
-    allocate(velocity(nGroups))
+    allocate(velocity(number_groups))
     read(5,*) velocity
     read(5,'(a)') materialName
-    read(5,*) nLegendre, dataPresent, energyFission, energyCapture, gramAtomWeight
+    read(5,*) number_legendre, dataPresent, energyFission, energyCapture, gramAtomWeight
     
     ! Make space for cross sections
-    allocate(sig_t(nMaterials, nGroups))
-    allocate(sig_f(nMaterials, nGroups))
-    allocate(vsig_f(nMaterials, nGroups))
-    allocate(chi(nMaterials, nGroups))
-    allocate(sig_s(nMaterials, nLegendre, nGroups, nGroups))
-    allocate(array1(nGroups))
+    allocate(sig_t(number_materials, number_groups))
+    allocate(sig_f(number_materials, number_groups))
+    allocate(vsig_f(number_materials, number_groups))
+    allocate(chi(number_materials, number_groups))
+    allocate(sig_s(number_materials, number_legendre, number_groups, number_groups))
+    allocate(array1(number_groups))
     
     ! Read the cross sections from the file
-    do mat = 1, nMaterials
+    do mat = 1, number_materials
       if (mat > 1) then  ! The first material was read above to get array sizes
         read(5,'(a)') materialName
-        read(5,*) nLegendre, dataPresent, energyFission, energyCapture, gramAtomWeight
+        read(5,*) number_legendre, dataPresent, energyFission, energyCapture, gramAtomWeight
       end if
-      do group = 1, nGroups
+      do group = 1, number_groups
         if (dataPresent == 1) then
           ! Read total and fission cross sections
           read(5,*) t, f, vf, c
@@ -56,10 +56,10 @@ module material
         end if
       end do
       ! Read scattering cross section
-      do L = 1, nLegendre
-        do group = 1, nGroups
+      do L = 1, number_legendre
+        do group = 1, number_groups
           read(5,*) array1
-          do groupp = 1, nGroups
+          do groupp = 1, number_groups
             sig_s(mat, L, group, groupp) = array1(groupp)
           end do
         end do
@@ -67,7 +67,7 @@ module material
     end do
       
     
-  end subroutine create
+  end subroutine create_material
 
 END module material
     
