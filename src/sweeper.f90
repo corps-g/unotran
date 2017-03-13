242503fe9620d6bf74f9d01673b95d449e309a0e
Richard <rlreed@ksu.edu>
2017-03-13 16:19:44 -0500

    Correcting and refactoring the sweep operator.  Now uses vector math for updating the sweeping source.  Additionally included is an update to the python function for plotting

--- src/sweeper.f90
+++ src/sweeper.f90
@@ -12,76 +12,79 @@ module sweeper
   
   subroutine sweep()
     integer :: o, c, a, g, gp, l, an, cmin, cmax, cstep, amin, amax, astep
-    double precision :: incoming(number_angles*2), Q, Ps, invmu
+    double precision :: incoming(number_groups,2*number_angles), Q(number_groups), Ps, invmu, fiss
     double precision :: phi_old(0:number_legendre,number_groups,number_cells)
+    logical :: octant
     
+    phi_old = phi
+    phi = 0.0  ! Reset phi
     do o = 1, 2  ! Sweep over octants
       ! Sweep in the correct direction in the octant
-      if (o .eq. 1) then
-        cmin = 1
-        cmax = number_cells
-        cstep = 1
-        amin = 1
-        amax = number_angles
-        astep = 1
-      else
-        cmin = number_cells
-        cmax = 1
-        cstep = -1
-        amin = number_angles
-        amax = 1
-        astep = -1
-      end if
+      octant = o .eq. 1
+      cmin = merge(1, number_cells, octant)
+      cmax = merge(number_cells, 1, octant)
+      cstep = merge(1, -1, octant)
+      amin = merge(1, number_angles, octant)
+      amax = merge(number_angles, 1, octant)
+      astep = merge(1, -1, octant)
       
-      phi_old = phi
-      phi = 0.0  ! Reset phi
+      
       incoming = 0.0  ! Set vacuum conditions for both faces
       do c = cmin, cmax, cstep  ! Sweep over cells
-        do g = 1, number_groups  ! Sweep over group
-          do a = amin, amax, astep  ! Sweep over angle
-            ! Get the correct angle index
-            if (o .eq. 1) then
-              an = a
-            else
-              an = 2 * number_angles - a + 1
-            end if
+        do a = amin, amax, astep  ! Sweep over angle
+          ! Get the correct angle index
+          an = merge(a, 2 * number_angles - a + 1, octant)
+          ! get a common fraction
+          invmu = dx(c) / (2 * abs(mu(a)))
           
-            ! get a common fraction
-            invmu = dx(c) / (2 * abs(mu(a)))
-            ! Update the right hand side
-            Q = source(g, an, c)
-            do gp = 1, number_groups
-              Q = Q + chi(g, mMap(c)) * vsig_f(gp, mMap(c)) * phi_old(0, gp, c)
-              do l = 0, number_legendre
-                Q = Q + (2 * l + 1) * p_leg(l, an) * sig_s(l, g, gp, mMap(c)) * phi_old(l, gp, c)
-              end do
-            end do
-            
-            call computeEQ(Q, incoming(an), sig_t(g, mMap(c)), invmu, incoming(an), Ps)
+          ! Update the right hand side
+          Q = updateSource(source(:,an,c), phi_old(:,:,c), c, an)
+          
+          do g = 1, number_groups  ! Sweep over group
+            ! Use the specified equation.  Defaults to DD
+            call computeEQ(Q(g), incoming(g,an), sig_t(g, mMap(c)), invmu, incoming(g,an), Ps)
             
             if (store_psi) then
               psi(g,an,c) = Ps
             end if
             
-            do l = 0, number_legendre
-              phi(l,g,c) = phi(l,g,c) + 0.5 * wt(a) * p_leg(l, an) * Ps
-            end do
+            ! Increment the legendre expansions of the scalar flux
+            phi(:,g,c) = phi(:,g,c) + 0.5 * wt(a) * p_leg(:, an) * Ps
           end do
         end do
       end do
     end do
   end subroutine sweep
   
-  subroutine computeEQ(Q, incoming, sig, invmu, outgoing, Ps)
+  subroutine computeEQ(Qg, incoming, sig, invmu, outgoing, Ps)
     implicit none
-    double precision, intent(in) :: Q, incoming, sig, invmu
+    double precision, intent(in) :: Qg, incoming, sig, invmu
     double precision, intent(out) :: outgoing, Ps
     
     if (equation .eq. 'DD') then
-      Ps = (incoming + invmu * Q) / (1 + invmu * sig)
+      ! Diamond Difference relationship
+      Ps = (incoming + invmu * Qg) / (1 + invmu * sig)
       outgoing = 2 * Ps - incoming
     end if
   
   end subroutine computeEQ
+  
+  function updateSource(Sg, Phig, cell, angle)
+    double precision :: updateSource(number_groups)
+    double precision, intent(in) :: Sg(number_groups), Phig(0:number_legendre, number_groups)
+    double precision :: scat(number_groups)
+    integer, intent(in) :: cell, angle
+    integer :: l
+    
+    ! Include the external source and the fission source
+    updateSource(:) = Sg(:) + chi(:, mMap(cell)) * dot_product(vsig_f(:, mMap(cell)), phig(0,:))
+    
+    ! Add the scattering source for each legendre moment
+    do l = 0, number_legendre
+      scat(:) = (2 * l + 1) * p_leg(l, angle) * matmul(sig_s(l, :, :, mMap(cell)), phig(l,:))
+      updateSource(:) = updateSource(:) + scat(:)
+    end do
+    
+  end function updateSource
   
 end module sweeper
