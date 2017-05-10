af07d2298782c9d3f88b1458301a006c6d73f444
Richard <rlreed@ksu.edu>
2017-05-10 16:16:52 -0500

    Removing extra function and debugging

--- src/dgmsweeper.f90
+++ src/dgmsweeper.f90
@@ -5,6 +5,7 @@ module dgmsweeper
   use mesh, only: dx, number_cells, mMap
   use angle, only: number_angles, p_leg, wt, mu
   use state, only: phi, psi, source, store_psi, equation
+  use sweeper, only : computeEQ
   use dgm
 
   implicit none
@@ -14,21 +15,19 @@ module dgmsweeper
   
   subroutine dgmsweep(lambda)
     integer :: o, c, a, g, gp, l, an, cmin, cmax, cstep, amin, amax, astep, cg, i
-    double precision :: incoming(number_groups,expansion_order,2*number_angles), Q(number_groups, expansion_order), Ps, invmu
+    double precision :: incoming(expansion_order,number_groups,2*number_angles), Q(expansion_order,number_groups), Ps, invmu
     double precision :: phi_old(0:number_legendre,number_groups,number_cells)
-    double precision :: S(number_course_groups,expansion_order), delta
+    double precision :: delta, num, M(0:number_legendre)
     double precision, intent(in) :: lambda
     logical :: octant
     
-    phi_old = phi
-    phi = 0.0  ! Reset phi
-    if (store_psi) then
-      psi = 0.0
-    end if
+    phi = (1.0 - lambda) * phi  ! Reset phi
+    psi = (1.0 - lambda) * psi  ! Reset psi
 
     do o = 1, 2  ! Sweep over octants
       ! Sweep in the correct direction in the octant
       octant = o .eq. 1
+      ! merge means : num = v1 if condition else v2
       cmin = merge(1, number_cells, octant)
       cmax = merge(number_cells, 1, octant)
       cstep = merge(1, -1, octant)
@@ -44,70 +43,50 @@ module dgmsweeper
           ! get a common fraction
           invmu = dx(c) / (2 * abs(mu(a)))
           
+          ! legendre polynomial integration vector
+          M = 0.5 * wt(a) * p_leg(:,an)
+
           ! Update the right hand side
           Q = updateSource(c, an)
+          do g = 1, number_groups  ! Sweep over group
+            cg = energyMesh(g)
 
-          do i = 1, expansion_order
-            do g = 1, number_groups  ! Sweep over group
-              cg = energyMesh(g)
-              if (i > order(cg)) then
-                cycle
-              end if
+            num = 0.0
+            do i = 1, order(cg)
               ! Use the specified equation.  Defaults to DD
-              delta = delta_moment(cg,i,an,c) * psi_0_moment(cg, an, c)
-              call computeEQ(Q(cg,i), incoming(cg,i,an), sig_t_moment(cg, mMap(c)), delta, invmu, incoming(cg,i,an), Ps)
+              call computeEQ(Q(i,cg), incoming(i,cg,an), sig_t_moment(cg, c), invmu, incoming(i,cg,an), Ps)
             
-              if (store_psi) then
-                psi(g,an,c) = psi(g,an,c) + basis(g,i) * Ps
-              end if
-            
-              ! Increment the legendre expansions of the scalar flux
-              phi(:,g,c) = phi(:,g,c) + 0.5 * wt(a) * p_leg(:, an) * basis(g,i) * Ps
+              num = num + lambda * basis(i,g) * Ps
             end do
+            ! compute the angular flux
+            psi(g,an,c) = psi(g,an,c) + num
+
+            ! Increment the legendre expansions of the scalar flux
+            phi(:,g,c) = phi(:,g,c) + M * num
           end do
         end do
       end do
     end do
 
-  phi = lambda * phi + (1 - lambda) * phi_old
-
   end subroutine dgmsweep
   
-  subroutine computeEQ(Qg, incoming, sig, delta, invmu, outgoing, Ps)
-    implicit none
-    double precision, intent(in) :: Qg, incoming, sig, invmu, delta
-    double precision, intent(out) :: outgoing, Ps
-    
-    if (equation .eq. 'DD') then
-      ! Diamond Difference relationship
-      Ps = (incoming + invmu * (Qg - delta)) / (1 + invmu * sig)
-      outgoing = 2 * Ps - incoming
-    end if
-  
-  end subroutine computeEQ
-  
-
   ! Compute the updated source term
   function updateSource(cell, angle)
-    double precision :: updateSource(number_course_groups,expansion_order)
+    double precision :: updateSource(expansion_order,number_course_groups)
     double precision :: num
     integer, intent(in) :: cell, angle
-    integer :: l, i, g, gp, cg
+    integer :: l, i, cg, cgp
     
     ! Include the external source and the fission source
     updateSource(:,:) = source_moment(:,:,angle,cell)
-    
     ! Add the scattering source for each legendre moment
-    do i = 1, expansion_order
-      do g = 1, number_course_groups
-        cg = energyMesh(g)
-        if (i > order(cg)) then
-          cycle
-        end if
-        do l = 0, number_legendre
-          do gp = 1, number_course_groups
-            num = (2 * l + 1) * p_leg(l, angle) * sig_s_moment(l, gp, g, i, cell) * phi_moment(l, gp, i, cell)
-            updateSource(g,i) = updateSource(g,i) + num
+    do cg = 1, number_course_groups
+      updateSource(:,cg) = updateSource(:,cg) - delta_moment(:,cg,angle,cell) * psi_0_moment(cg, angle, cell)
+      do cgp = 1, number_course_groups
+        do i = 1, order(cg)
+          do l = 0, number_legendre
+            num = (2 * l + 1) * p_leg(l, angle) * sig_s_moment(l, i, cgp, cg, cell) * phi_moment(l, 1, cgp, cell)
+            updateSource(i,cg) = updateSource(i,cg) + num
           end do
         end do
       end do
