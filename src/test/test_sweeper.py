eb9da0aed0d3b4dc6b3957eacef67d358cb9a16c
Richard Reed <rlreed@ksu.edu>
2018-01-11 14:59:03 -0600

    Updating the sweeper test to account for the change in sweeper

--- src/test/test_sweeper.py
+++ src/test/test_sweeper.py
@@ -40,7 +40,7 @@ class TestSWEEPER(unittest.TestCase):
         Ps = 0.0
         inc = np.array([0.0])
         
-        Ps = pydgm.sweeper.computeeq(1.0, inc, 1.0, frac, 1.0, 0.8611363115940526)
+        Ps = pydgm.sweeper.computeeq(1.0, inc, 1.0, 1.0, 0.8611363115940526)
         
         self.assertAlmostEqual(inc[0], 0.734680275209795978, 12)
         self.assertAlmostEqual(Ps, 0.367340137604897989, 12)
@@ -55,7 +55,7 @@ class TestSWEEPER(unittest.TestCase):
         Ps = 0.0
         inc = np.array([0.0])
         
-        Ps = pydgm.sweeper.computeeq(1.0, inc, 1.0, frac, 1.0, 0.8611363115940526)
+        Ps = pydgm.sweeper.computeeq(1.0, inc, 1.0, 1.0, 0.8611363115940526)
         
         self.assertAlmostEqual(inc[0], 0.686907416523104323, 12)
         self.assertAlmostEqual(Ps, 0.408479080928661801, 12)
