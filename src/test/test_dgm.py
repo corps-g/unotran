Fixing problem with the source term.  It was not being indexed correctly in sweeper, and for the dgm case, was too small by a factor of 2

src/test/test_dgm.py
@@ -117,24 +117,26 @@ class TestDGM(unittest.TestCase):
         '''
         phi = np.array([0.198933535568562, 2.7231683533646702, 1.3986600409998782, 1.010361903429942, 0.8149441787223116, 0.8510697418684054, 0.00286224604623])
         for a in range(4):
-            pydgm.state.psi[0,a,:] = phi / 2
-            
-        test = np.array([[1.3327809583407633, -0.1240768172509027, -0.728133238841511, -0.5349740429521677],
-                         [0.4817630520259961,  0.2871143207371673, -0.1805137297622373, 0.0]])
+            pydgm.state.psi[0, a, :] = phi / 2
+        basis = np.loadtxt('test/7gbasis').T
+        test = basis.dot(phi) * 0.5
+        test.resize(2, 4)
+        psi = np.reshape(np.resize(phi, (1, 4, 7)), (1, 4, 7), order='F') * 0.5
+
         for i in range(4):
-            pydgm.dgmsolver.compute_incoming_flux(i)
+            pydgm.dgmsolver.compute_incoming_flux(i, psi)
             for a in range(2):
-                np.testing.assert_array_almost_equal(pydgm.state.d_incoming[a,:], test[:,i], 12)
+                np.testing.assert_array_almost_equal(pydgm.state.d_incoming[a, :], test[:, i], 12)
 
     def test_dgm_compute_xs_moments(self):
         '''
         Check that the XS moments are properly computed
         '''
         sig_t_m_test = [0.3760865, 1.0070863333333333]
-        delta_m_test = np.array([0.0, -0.12284241926631045, 0.00018900000000000167, 1.0668056713853770e-02, 
-                                 0.0, -4.8916473462696236e-01, 2.0934839066069319e-01, 0.0, 
+        delta_m_test = np.array([0.0, -0.12284241926631045, 0.00018900000000000167, 1.0668056713853770e-02,
+                                 0.0, -4.8916473462696236e-01, 2.0934839066069319e-01, 0.0,
                                  0.0, -1.2284241926631045e-01, 1.8900000000000167e-04, 1.0668056713853770e-02,
-                                 0.0, -4.8916473462696236e-01, 2.0934839066069319e-01, 0.0, 
+                                 0.0, -4.8916473462696236e-01, 2.0934839066069319e-01, 0.0,
                                  0.0, -1.2284241926631045e-01, 1.8900000000000167e-04, 1.0668056713853770e-02,
                                  0.0, -4.8916473462696236e-01, 2.0934839066069319e-01, 0.0,
                                  0.0, -1.2284241926631045e-01, 1.8900000000000167e-04, 1.0668056713853770e-02,
@@ -143,15 +145,15 @@ class TestDGM(unittest.TestCase):
         nu_sig_f_m_test = [0.039245915, 1.504587272273979]
 
         pydgm.dgmsolver.compute_flux_moments()
+
         pydgm.dgmsolver.compute_source_moments()
         pydgm.dgmsolver.compute_xs_moments()
         np.set_printoptions(precision=5, linewidth=132, suppress=True)
-        
+
         for i in range(pydgm.dgm.expansion_order + 1):
             pydgm.dgmsolver.slice_xs_moments(i)
-            S = pydgm.dgm.source_m[0,:,:,i].flatten()
-            np.testing.assert_array_almost_equal(S - pydgm.state.d_source.flatten(), delta_m_test[:, :, i].flatten(), 12)
-            np.testing.assert_array_almost_equal(pydgm.state.d_sig_s.flatten(), sig_s_m_test[i].flatten(), 12)
+            np.testing.assert_array_almost_equal(pydgm.dgm.delta_m[0].flatten(), delta_m_test.flatten(), 12)
+            np.testing.assert_array_almost_equal(pydgm.dgm.sig_s_m[:, :, :, :, i].flatten(), sig_s_m_test[i].flatten(), 12)
             np.testing.assert_array_almost_equal(pydgm.state.d_sig_t.flatten(), sig_t_m_test)
             np.testing.assert_array_almost_equal(pydgm.state.d_nu_sig_f.flatten(), nu_sig_f_m_test)
 
@@ -252,7 +254,7 @@ class TestDGM2(unittest.TestCase):
             pydgm.dgmsolver.slice_xs_moments(i)
             for a in range(4):
                 np.testing.assert_array_almost_equal(pydgm.dgm.delta_m[0, a, :, i].flatten(), delta_m_test[:, i].flatten(), 12)
-            np.testing.assert_array_almost_equal(pydgm.state.d_sig_s.flatten('F'), sig_s_m_test[:, :, i].flatten(), 12)
+            np.testing.assert_array_almost_equal(pydgm.dgm.sig_s_m[:, :, :, :, i].flatten('F'), sig_s_m_test[:, :, i].flatten(), 12)
             np.testing.assert_array_almost_equal(pydgm.state.d_sig_t.flatten(), sig_t_m_test)
             np.testing.assert_array_almost_equal(pydgm.state.d_nu_sig_f.flatten(), nu_sig_f_m_test)
 

src/test/test_solver.py
@@ -59,7 +59,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_inf_med_1g(self):
@@ -95,7 +95,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_inf_med_2g(self):
@@ -131,7 +131,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_inf_med_7g(self):
@@ -168,7 +168,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_2med_ref_1g(self):
@@ -203,7 +203,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_2med_ref_2g(self):
@@ -238,7 +238,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_2med_ref_7g(self):
@@ -277,7 +277,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenV1g(self):
@@ -318,7 +318,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenV2g(self):
@@ -359,7 +359,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenV7g(self):
@@ -399,7 +399,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR1g(self):
@@ -451,7 +451,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR2g(self):
@@ -503,7 +503,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR7g(self):
@@ -555,7 +555,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR1gPin(self):
@@ -598,7 +598,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR2gPin(self):
@@ -641,7 +641,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     def test_solver_eigenR7gPin(self):
@@ -683,7 +683,7 @@ class TestSOLVER(unittest.TestCase):
         for c in range(pydgm.control.number_cells):
             for a in range(nAngles):
                 phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, a, :]
-                phi_test[c] += pydgm.angle.wt[nAngles - a - 1] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
+                phi_test[c] += pydgm.angle.wt[a] * pydgm.state.psi[c, 2 * nAngles - a - 1, :]
         np.testing.assert_array_almost_equal(pydgm.state.phi[0, :, :], phi_test, 12)
 
     # TODO: Add anisotropic tests

src/test/test_sweeper.py
@@ -87,7 +87,7 @@ class TestSWEEPER(unittest.TestCase):
         pydgm.sweeper.sweep(g, source, phi_g, psi_g, incident)
         
         np.testing.assert_array_almost_equal(phi_g, 2.4702649838962234, 12)
-        psi_test = np.array([0.74789724066688767, 1.0164427642376821, 1.1738899625537731, 1.7463807889213092])
+        psi_test = np.array([0.74789724066688767, 1.0164427642376821, 1.7463807889213092, 1.1738899625537731])
         np.testing.assert_array_almost_equal(psi_g.flatten(), psi_test, 12)
         np.testing.assert_array_almost_equal(incident, [1.3519854437737708, 1.9598760493672542])
         
@@ -106,7 +106,7 @@ class TestSWEEPER(unittest.TestCase):
         pydgm.sweeper.sweep(g, source, phi_g, psi_g, incident)
         
         np.testing.assert_array_almost_equal(phi_g, 1.0863050345964158, 12)
-        psi_test = np.array([0.31829108536954637, 0.6630938187058939, 0.31829108536954637, 0.6630938187058939])
+        psi_test = np.array([0.31829108536954637, 0.6630938187058939, 0.6630938187058939, 0.31829108536954637])
         np.testing.assert_array_almost_equal(psi_g.flatten(), psi_test, 12)
         np.testing.assert_array_almost_equal(incident, [0.6365821707390927, 1.3261876374117878])
         