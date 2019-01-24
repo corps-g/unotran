89898c57d70cc7b36317786b9d292ff8e79aa63f
Richard Reed <rlreed@ksu.edu>
2019-01-24 16:21:27 -0600

    Adding the sph_demo

---
+++ SPH/example1.py
@@ -0,0 +1,159 @@
+import numpy as np
+from sph import XS, DGMSOLVER
+import matplotlib.pyplot as plt
+
+
+def buildGEO(pin_map, homogenzied=False):
+    fine_map = [9, 12, 9]
+    coarse_map = [0.0, 0.45, 1.05, 1.5]
+    material_map = [[4, 1, 4], [4, 2, 4], [4, 3, 4], [4, 4, 4]]  # High UO2 | Low UO2 | RCC | Water
+
+    npins = len(pin_map)
+
+    if homogenzied:
+        fine_map = [10]
+        coarse_map = [coarse_map[0], coarse_map[-1]]
+        material_map = [[i + 1] for i in range(npins)]
+        pin_map = range(npins)
+
+    fm = fine_map * npins
+    cm = [xx + i * coarse_map[-1] for i in range(npins) for xx in coarse_map[:-1]] + [npins * coarse_map[-1]]
+    mm = []
+    for p in pin_map:
+        mm += material_map[p]
+
+    return npins, fm, cm, mm
+
+
+def plot(ref, homo):
+    '''
+    Create a plot comparing the reference array to the homogenized one
+    '''
+
+    ncells = len(ref.phi[0]) / len(ref.phi_homo[0])
+
+    rxn = ref.phi * (ref.sig_t - ref.sig_s)
+    rxn_homo = ref.phi_homo * (ref.sig_t_homo - ref.sig_s_homo)
+    rxn_sph = homo.phi_homo * (homo.sig_t_homo - homo.sig_s_homo)
+
+    plt.figure(0)
+    plt.plot(rxn.flatten(), label='reference')
+    plt.plot([h for h in rxn_homo.flatten() for _ in range(ncells)], label='homogenized')
+    plt.plot([h for h in rxn_sph.flatten() for _ in range(ncells)], label='sph')
+    plt.legend()
+    plt.xlabel('spatial cell')
+    plt.ylabel('absorption rate')
+
+    plt.figure(1)
+    plt.plot(ref.phi.flatten(), label='reference')
+    plt.plot([h for h in ref.phi_homo.flatten() for _ in range(ncells)], label='homogenized')
+    plt.plot([h for h in homo.phi_homo.flatten() for _ in range(ncells)], label='sph')
+    plt.legend()
+    plt.xlabel('spatial cell')
+    plt.ylabel('scalar flux')
+
+    plt.show()
+
+
+def runSPH(G, pin_map, xs_name):
+    '''
+    Run the SPH problem 
+    
+    Inputs:
+        G       - Number of groups in the problem
+        pin_map - List of pins in the geometry
+        xs_name - Str with the cross section file in anlxs format
+    '''
+
+    # Build the reference geometry
+    nPin, fm, cm, mm = buildGEO(pin_map, False)
+
+    # Create the initial SPH factors
+    old_mu = np.ones((G, nPin))
+    fname = '_homo.'.join(xs_name.split('.'))
+
+    # Solve for the reference problem
+    ref = DGMSOLVER(G, xs_name, fm, cm, mm, nPin)
+    ref_XS = XS(ref.sig_t_homo, ref.sig_f_homo, ref.chi_homo, ref.sig_s_homo)
+
+    # Write the reference cross sections
+    ref_XS.write_homogenized_XS(fname, old_mu)
+
+    # Build the homogenized geometry
+    nPin, fm, cm, mm = buildGEO(pin_map, True)
+
+    for i in range(1000):
+        # Get the homogenized solution
+        homo = DGMSOLVER(G, fname, fm, cm, mm, nPin, ref.norm)
+
+        # Compute the SPH factors
+        mu = ref.phi_homo / homo.phi_homo  # / np.linalg.norm(ref.phi_homo, axis=1) * np.linalg.norm(homo.phi_homo, axis=1)
+
+        # Write the new cross sections adjusted by SPH
+        ref_XS.write_homogenized_XS(fname, mu)
+
+        # Compute the error between successive iterations
+        err = np.linalg.norm(mu - old_mu, np.inf)
+
+        # Save the previous SPH factors
+        old_mu = np.copy(mu)
+
+        # Provide iteration output
+        print 'Iter: {}    Error: {}'.format(i + 1, err)
+        if err < 1e-6:
+            break
+
+    print 'SPH factors'
+    print mu
+
+    rxn_ref = ref.phi_homo * (ref.sig_t_homo - ref.sig_s_homo)
+    rxn_homo = homo.phi_homo * (homo.sig_t_homo - homo.sig_s_homo)
+    print 'Make sure these reactions rates are the same if SPH is working properly'
+    print rxn_ref
+    print rxn_homo
+
+    return ref_XS
+
+
+def makeColorset(G, pin_map, xs_name, homog=False, norm=None):
+    nPin, fm, cm, mm = buildGEO(pin_map, homog)
+
+    S = DGMSOLVER(G, xs_name, fm, cm, mm, nPin, norm)
+
+    return S
+
+
+if __name__ == '__main__':
+    np.set_printoptions(precision=3)
+    xs_name = 'XS/colorset.anlxs'
+
+    # Get the reference solution
+    pin_map = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    ref = makeColorset(1, pin_map, xs_name, False)
+
+    # Get the homogenized cross sections
+    uo2low = runSPH(1, [1, 1, 1, 1, 1, 1, 1, 1], xs_name)
+    uo2high = runSPH(1, [0, 0, 0, 0, 0, 0, 0, 0], xs_name)
+    uo2XS = uo2low + uo2high
+
+    # Write the SPH cross sections
+    uo2XS.write_homogenized_XS('XS/colorset_homogenized.anlxs')
+
+    # Get the homogenized solution
+    homo = makeColorset(1, pin_map, 'XS/colorset_homogenized.anlxs', True)
+
+    rxn_ref = ref.phi_homo * (ref.sig_t_homo - ref.sig_s_homo)
+    rxn_homo = homo.phi_homo * (homo.sig_t_homo - homo.sig_s_homo)
+
+    ref_errs = -np.array([-0.07, -0.1, -0.16, -0.26, -0.43, -0.69, -1.00, -1.22, 1.57, 0.9, 0.45, 0.22, 0.1, 0.04, 0.02, 0.02])
+
+    print 'Reference reaction rates'
+    print rxn_ref
+    print 'Homogenzied reaction rates'
+    print rxn_homo
+    print ref_errs / 100 * rxn_ref + rxn_ref
+    np.set_printoptions(precision=2, suppress=True)
+    print 'Error in reaction rates'
+    print (rxn_homo - rxn_ref) / rxn_ref * 100
+
+    plot(ref, homo)
---
+++ SPH/sph.py
@@ -0,0 +1,181 @@
+import pydgm
+import numpy as np
+
+
+class XS():
+
+    def __init__(self, sig_t, sig_f, chi, sig_s, mu=None):
+        self.sig_t = sig_t
+        self.sig_f = sig_f
+        self.chi = chi
+        self.sig_s = sig_s
+        if mu is None:
+            self.mu = np.ones(self.sig_t.shape)
+        else:
+            self.mu = mu
+
+    def write_homogenized_XS(self, fname, mu=None):
+        if mu is not None:
+            assert mu.shape == self.sig_t.shape
+            self.mu = mu
+
+        G, npin = self.sig_t.shape
+
+        sig_t = self.sig_t * self.mu
+        vsig_f = self.sig_f * self.mu
+        sig_s = self.sig_s * self.mu
+
+        # Write the cross sections to file
+        s = '{} {} 0\n'.format(npin, G)
+        s += '{}\n'.format(' '.join([str(g) for g in range(G + 1)]))
+        s += '{}\n'.format(' '.join([str(g) for g in range(G)]))
+        for mat in range(npin):
+            s += 'pin {}\n'.format(mat + 1)
+            s += '1 1 1.0 0.0 0.602214179\n'
+
+            for g in range(G):
+                s += '{:<12.9f} {:<12.9f} {:<12.9f} {:<12.9f}\n'.format(sig_t[g, mat], vsig_f[g, mat], vsig_f[g, mat], self.chi[g, mat])
+            for g in range(G):
+                s += '{}\n'.format(' '.join(['{:<12.9f}'.format(s) for s in sig_s[:, g, mat]]))
+
+        with open(fname, 'w') as f:
+            f.write(s[:-1])
+
+    def __add__(self, newXS):
+        sig_t = np.concatenate([self.sig_t, newXS.sig_t], axis=-1)
+        sig_f = np.concatenate([self.sig_f, newXS.sig_f], axis=-1)
+        sig_s = np.concatenate([self.sig_s, newXS.sig_s], axis=-1)
+        chi = np.concatenate([self.chi, newXS.chi], axis=-1)
+        mu = np.concatenate([self.mu, newXS.mu], axis=-1)
+
+        return XS(sig_t, sig_f, chi, sig_s, mu)
+
+
+class DGMSOLVER():
+
+    # Solve the problem specified for the options
+    def __init__(self, G, fname, fm, cm, mm, nPin, norm=None):
+        '''
+        Inputs:
+            G     - Number of energy groups
+            fname - Name of the cross section file 
+            fm    - Fine mesh
+            cm    - Coarse mesh
+            mm    - Material map 
+            nPin  - Number of pincells 
+        '''
+
+        self.G = G
+        self.fname = fname
+        self.fm = fm
+        self.cm = cm
+        self.mm = mm
+        self.npin = nPin
+        self.norm = norm
+
+        # Pass on the options to unotran
+        self.setOptions()
+
+        # Solve using unotran
+        self.solve()
+
+        # Homogenize the cross sections over each pin cell
+        self.homogenize()
+
+    def setOptions(self):
+        '''
+        Set the options for the Unotran solve
+        '''
+
+        pydgm.control.fine_mesh = self.fm
+        pydgm.control.coarse_mesh = self.cm
+        pydgm.control.material_map = self.mm
+        pydgm.control.xs_name = self.fname.ljust(256)
+        pydgm.control.angle_order = 1
+        pydgm.control.angle_option = pydgm.angle.gl
+        pydgm.control.boundary_type = [1.0, 1.0]
+        pydgm.control.allow_fission = False
+        pydgm.control.eigen_print = 0
+        pydgm.control.outer_print = 0
+        pydgm.control.inner_print = 0
+        pydgm.control.eigen_tolerance = 1e-16
+        pydgm.control.outer_tolerance = 1e-16
+        pydgm.control.inner_tolerance = 1e-16
+        pydgm.control.max_eigen_iters = 1000
+        pydgm.control.max_outer_iters = 1000
+        pydgm.control.store_psi = True
+        pydgm.control.solver_type = 'fixed'.ljust(256)
+        pydgm.control.source_value = 1.0
+        pydgm.control.equation_type = 'DD'
+        pydgm.control.scatter_legendre_order = 0
+        pydgm.control.ignore_warnings = True
+
+    def solve(self):
+        '''
+        Solve the problem using Unotran
+        '''
+
+        # Initialize the problem
+        pydgm.solver.initialize_solver()
+
+        # Call the solver
+        pydgm.solver.solve()
+
+        # Copy any information from Unotran
+        self.extractInfo()
+
+        # Clean up the solver
+        pydgm.solver.finalize_solver()
+        pydgm.control.finalize_control()
+
+    def extractInfo(self):
+        '''
+        Copy information from Unotran before the solver is deallocated
+        '''
+
+        self.phi = np.copy(pydgm.state.phi[0])
+        self.dx = np.copy(pydgm.mesh.dx)
+        self.mat_map = np.copy(pydgm.state.mg_mmap)
+        self.sig_t = np.array([pydgm.material.sig_t[:, self.mat_map[c] - 1] for c in range(len(self.mat_map))]).T
+        self.sig_s = np.array([pydgm.material.sig_s[0, :, :, self.mat_map[c] - 1] for c in range(len(self.mat_map))]).T
+        self.vsig_f = np.array([pydgm.material.nu_sig_f[:, self.mat_map[c] - 1] for c in range(len(self.mat_map))]).T
+        self.chi = np.array([pydgm.material.chi[:, self.mat_map[c] - 1] for c in range(len(self.mat_map))]).T
+
+    def homogenize(self):
+        '''
+        Homogenize the cross sections over the pincell
+        '''
+
+        def homosum(array):
+            '''Convenience function to make things shorter'''
+            return np.sum((array).reshape(-1, self.npin, nCellPerPin), axis=2) / l
+
+        # Check that everything is the right shape of arrays
+        shape = self.phi.shape
+        assert shape[0] == self.G
+        assert (shape[1] / self.npin) == (shape[1] // self.npin)
+
+        # Compute the number of pins
+        nCellPerPin = shape[1] // self.npin
+
+        # Homogenize the flux
+        l = np.sum(self.dx.reshape(self.npin, -1), axis=1)
+        d_phi = self.phi[:, :] * self.dx[:]
+        self.phi_homo = homosum(d_phi)
+
+        # Either find the norm of the flux or normalize the flux to self.norm
+        if self.norm is None:
+            self.norm = np.sum(d_phi)
+        else:
+            norm = self.phi_homo.dot(l)
+            self.phi_homo *= self.norm / norm
+            d_phi *= self.norm / norm
+
+        # Homogenize the cross sections
+        self.sig_t_homo = homosum(d_phi * self.sig_t) / self.phi_homo
+        self.sig_f_homo = homosum(d_phi * self.vsig_f) / self.phi_homo
+        self.chi_homo = homosum(self.chi * self.dx)
+        self.sig_s_homo = np.zeros((self.G, self.G, self.npin))
+        for gp in range(self.G):
+            self.sig_s_homo[gp] = homosum(d_phi * self.sig_s[gp]) / self.phi_homo
+
